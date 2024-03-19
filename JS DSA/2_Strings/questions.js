e4782a05184d356dd62e5fc0b5cd952eb8976471
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-03-19 13:52:27 +0530

    String Questions in JS

---
+++ JS DSA/2_Strings/questions.js
@@ -0,0 +1,191 @@
+// Find the Index of the First Occurrence in a String
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+var strStr = function (haystack, needle) {
+  // first of all we will find the first character of needle inside haystack
+  // if its found then we check substring from that index to needle.length and match with needle
+  // else we return -1
+  // we will run the loop till haystack.length - needle.length + 1 because of out of bound condition
+  for (let j = 0; j < haystack.length - needle.length + 1; j++) {
+    if (haystack.charAt(j) == needle.charAt(0)) {
+      let substr = haystack.substring(j, j + needle.length);
+      if (needle === substr) {
+        return j;
+      }
+    }
+  }
+
+  return -1;
+};
+
+// Reverse String
+// Write a function that reverses a string. The input string is given as an array of characters s. You must do this by modifying the input array in-place with O(1) extra memory
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (i <= j) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+
+  return s;
+};
+
+// Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise. An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let letters = {};
+  //create hashmap for both words, based on a counter
+  for (let i = 0; i < s.length; i++) {
+    letters[s[i]] = letters[s[i]] ? letters[s[i]] + 1 : 1;
+    letters[t[i]] = letters[t[i]] ? letters[t[i]] - 1 : -1;
+  }
+
+  for (let letter in letters) {
+    //this check for duplicates since if all letters are the same, we will end up with 0
+    if (letters[letter] !== 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+var isAnagram = function (s, t) {
+  // using in-built functions
+  return s.split("").sort().join("") === t.split("").sort().join("");
+};
+
+//  Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+var longestCommonPrefix = function (strs) {
+  // Take the smallest string and take prefix = ""
+  // add one character each time from smallests string in prefix and
+  // check if prefix is present in other strings or not
+  let smallestString = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length <= smallestString.length) {
+      smallestString = strs[i];
+    }
+  }
+
+  console.log(smallestString);
+
+  let prefix = "";
+  for (let j = 0; j < smallestString.length; j++) {
+    for (let k = 0; k < strs.length; k++) {
+      if (strs[k] == smallestString) {
+        continue;
+      }
+
+      if (strs[k][j] != smallestString[j]) {
+        return prefix;
+      }
+    }
+    prefix += smallestString[j];
+  }
+
+  return prefix;
+};
+
+// Merge Strings Alternately
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string. Return the merged string.
+var mergeAlternately = function (word1, word2) {
+  let str1 = word1.split("");
+  let str2 = word2.split("");
+
+  let res = "";
+  let i = 0;
+  let j = 0;
+  let k = 1;
+  while (i < str1.length && j < str2.length) {
+    if (k % 2 == 0) {
+      res += str2[j++];
+    } else {
+      res += str1[i++];
+    }
+    k++;
+  }
+
+  while (i < str1.length) {
+    res += str1[i++];
+  }
+
+  while (j < str2.length) {
+    res += str2[j++];
+  }
+
+  return res;
+};
+
+// Length of last word
+// Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only.
+var lengthOfLastWord = function (s) {
+  // Trim all the whitespaces from the string
+  const trimString = s.trim();
+  // Now split using " "
+  const splitString = trimString.split(" ");
+  // As its now converted to any array so get the length of last index of that array
+  return splitString[splitString.length - 1].length;
+};
+
+// Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers. Given a string s, return true if it is a palindrome, or false otherwise.
+var isPalindrome = function (s) {
+  let str = "";
+  // store all non alpha-numeric letters in a string with ignoring all white spaces
+  for (let i = 0; i < s.length; i++) {
+    if (
+      (s[i].toLowerCase() >= "a" && s[i].toLowerCase() <= "z") ||
+      (s[i].toLowerCase() >= "A" && s[i].toLowerCase() <= "Z") ||
+      (s[i].toLowerCase() >= "0" && s[i].toLowerCase() <= "9" && str[i] != " ")
+    ) {
+      str += s[i].toLowerCase();
+    }
+  }
+
+  // check if that string is an palindrome or not
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i <= j) {
+    if (str[i] != str[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+
+  return true;
+};
+
+// String Compression
+// Given an array of characters chars, compress it using the following algorithm: Begin with an empty string s. For each group of consecutive repeating characters in chars: If the group's length is 1, append the character to s. Otherwise, append the character followed by the group's length. The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars. After you are done modifying the input array, return the new length of the array. You must write an algorithm that uses only constant extra space.
+// Input: chars = ["a","a","b","b","c","c","c"]
+// Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+// Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+var compress = function (temp) {
+  let res = "";
+  let count = 0;
+  for (let i = 0; i < temp.length; i++) {
+    count++;
+    // if its unique character, add it in the string else count it
+    if (temp[i] != temp[i + 1]) {
+      res = res + temp[i] + count;
+      // now new character starts so make count = 0
+      count = 0;
+    }
+  }
+
+  return res;
+};
+
+// Reverse words in a string
+// Given an input string s, reverse the order of the words. A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space. Return a string of the words in reverse order concatenated by a single space. Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
