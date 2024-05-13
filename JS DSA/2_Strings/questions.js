b6a825d2bb33becc8dc782fbba3869149cacca5f
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-05-13 09:16:25 +0530

    String Extra Question

--- JS DSA/2_Strings/questions.js
+++ JS DSA/2_Strings/questions.js
@@ -256,3 +256,69 @@ var rotateString = function (s, goal) {
 
   return false;
 };
+
+// Compare Version Numbers
+/*
+Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+
+To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+
+Return the following:
+
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+Otherwise, return 0.
+
+Example 1:
+Input: version1 = "1.2", version2 = "1.10"
+Output: -1
+Explanation:
+version1's second revision is "2" and version2's second revision is "10": 2 < 10, so version1 < version2.
+
+Example 2:
+Input: version1 = "1.01", version2 = "1.001"
+Output: 0
+Explanation:
+Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+*/
+var compareVersion = function (version1, version2) {
+  // Each version number contains 2.45.64.23.67
+  // and 2,45,64,23,67 are revisions
+  // Each revision is separated by a '.'
+  // Ignore any leading zeroes in any revision means 001 = 1 or 00004 = 4 only
+  // if there is no digit, consider it as 0
+  // we need to start comparing from left -> right
+  // if v1>v2 return 1, v2>v1 return -1 else return 0
+  // we will take the number before . everytime from both strings and compare then
+  let v1length = version1.length;
+  let v2length = version2.length;
+  if (v1length == 0) {
+    return -1;
+  } else if (v2length == 0) {
+    return 1;
+  }
+
+  let i = 0;
+  let j = 0;
+  while (i < v1length || j < v2length) {
+    let n1 = 0,
+      n2 = 0;
+    while (i < v1length && version1[i] != ".") {
+      n1 = n1 * 10 + (version1[i] - "0");
+      i++;
+    }
+
+    while (j < v2length && version2[j] != ".") {
+      n2 = n2 * 10 + (version2[j] - "0");
+      j++;
+    }
+
+    if (n1 > n2) return 1;
+    else if (n2 > n1) return -1;
+
+    i++;
+    j++;
+  }
+
+  return 0;
+};
