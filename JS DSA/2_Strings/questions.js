4f015a30da5ce706d7d34e3918a257d836d07171
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-06-07 16:55:55 +0530

    String Extra Question

--- JS DSA/2_Strings/questions.js
+++ JS DSA/2_Strings/questions.js
@@ -460,3 +460,32 @@ var commonChars = function (words) {
 
   return res;
 };
+
+// Replace Words
+/*
+In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+Return the sentence after the replacement.
+
+Example 1:
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+*/
+var replaceWords = function (dictionary, sentence) {
+  // Approach: We have to find words in sentence which are there in our dictionary and we need to replace them
+  // We put all words in sentence in an array and take each word and map it with all characters in dictionary
+  // if we have found it, replce that word with dictionaty[i]
+  // At the end return the string as it is by using arr.reduce((accumulator, currentWord) => acc += word)
+  let wordArr = sentence.split(" ");
+
+  wordArr = wordArr.map((w) => {
+    for (let i = 0; i <= dictionary.length - 1; i++) {
+      if (w.indexOf(dictionary[i]) === 0) {
+        w = dictionary[i];
+      }
+    }
+    return w;
+  });
+  return wordArr.reduce((str, word) => (str += `${word} `), "").trim();
+};
