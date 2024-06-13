85869c14d91c677acc1dbb9f1aa831926f3ca474
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-06-13 10:46:25 +0530

    Sorting Extra Question Added

--- JS DSA/6_Sorting/questions.js
+++ JS DSA/6_Sorting/questions.js
@@ -303,3 +303,59 @@ var relativeSortArray = function (arr1, arr2) {
   }
   return arr;
 };
+
+// Minimum Number of Moves to Seat Everyone
+/*
+There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+
+You may perform the following move any number of times:
+
+Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+Note that there may be multiple seats or students in the same position at the beginning.
+
+Example 1:
+Input: seats = [3,1,5], students = [2,7,4]
+Output: 4
+Explanation: The students are moved as follows:
+- The first student is moved from from position 2 to position 1 using 1 move.
+- The second student is moved from from position 7 to position 5 using 2 moves.
+- The third student is moved from from position 4 to position 3 using 1 move.
+In total, 1 + 2 + 1 = 4 moves were used.
+
+Example 2:
+Input: seats = [4,1,5,9], students = [1,3,2,6]
+Output: 7
+Explanation: The students are moved as follows:
+- The first student is not moved.
+- The second student is moved from from position 3 to position 4 using 1 move.
+- The third student is moved from from position 2 to position 5 using 3 moves.
+- The fourth student is moved from from position 6 to position 9 using 3 moves.
+In total, 0 + 1 + 3 + 3 = 7 moves were used.
+
+Example 3:
+Input: seats = [2,2,6,6], students = [1,3,2,6]
+Output: 4
+Explanation: Note that there are two seats at position 2 and two seats at position 6.
+The students are moved as follows:
+- The first student is moved from from position 1 to position 2 using 1 move.
+- The second student is moved from from position 3 to position 6 using 3 moves.
+- The third student is not moved.
+- The fourth student is not moved.
+In total, 1 + 3 + 0 + 0 = 4 moves were used.
+*/
+var minMovesToSeat = function (seats, students) {
+  // We will sort both students and seats
+  // We will try to allocate first seat available to first student
+  // If ith seat != jth student means move jth student to some places and make him seat
+  // Store that count
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < students.length; i++) {
+    count += Math.abs(students[i] - seats[i]);
+  }
+
+  return count;
+};
