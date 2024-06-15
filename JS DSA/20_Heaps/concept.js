f8007cf9f5885e417edeaea99dab3c2f54e20ed0
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-06-15 20:32:30 +0530

    Heaps Extra Question

--- JS DSA/20_Heaps/concept.js
+++ JS DSA/20_Heaps/concept.js
@@ -429,3 +429,60 @@ var findRelativeRanks = function (score) {
 
   return ans;
 };
+
+// IPO
+/*
+Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
+
+You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+
+Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+
+Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+
+The answer is guaranteed to fit in a 32-bit signed integer.
+
+Example 1:
+Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+Output: 4
+Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+After finishing it you will obtain profit 1 and your capital becomes 1.
+With capital 1, you can either start the project indexed 1 or the project indexed 2.
+Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+
+Example 2:
+Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+Output: 6
+*/
+var findMaximizedCapital = function (k, w, profits, capital) {
+  // Start with a list of project sorted by capital cost ascending and a priority queue.
+  // Before choosing the next project, enqueue jobs until the the capital cost exceeds your operating capital w.
+  // dequeue the most profitable project and add it to the operating capital.
+  // Repeat k times or until all the projects are run.
+  /* Sort the projects by capital cost. */
+  let projects = [];
+  for (let i = 0; i < profits.length; i++)
+    projects.push([profits[i], capital[i]]);
+  projects.sort((a, b) => a[1] - b[1]);
+
+  /* Run the projects. */
+  let pq = new MaxPriorityQueue(),
+    pp = 0;
+  while (k > 0) {
+    /* Add all the projects fundable with the current operating capital. */
+    while (pp < projects.length && projects[pp][1] <= w) {
+      pq.enqueue(pp, projects[pp][0]);
+      pp++;
+    }
+
+    if (pq.size() === 0) break;
+
+    /* Choose the most profitable available project. */
+    let el = pq.dequeue();
+    w += el.priority;
+    k--;
+  }
+
+  return w;
+};
