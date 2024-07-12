c96e5cefc79299df31406a53389f2807670e1add
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-07-12 20:43:18 +0530

    Stack extra question

--- JS DSA/9_Stack/questions.js
+++ JS DSA/9_Stack/questions.js
@@ -1012,3 +1012,83 @@ function reverseString(s) {
   // console.log(returnString);
   return returnString;
 }
+
+// Maximum Score From Removing Substrings
+/*
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+
+Example 1:
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+
+Example 2:
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+*/
+var maximumGain = function (s, x, y) {
+  // We will follow a greedy approaach
+  // Where we check if x>y or y>x
+  // accordingly we first collect all 'ab' or 'ba' based on x>y or y>x because we want to maximize our points so we will store maximum vale first
+  // then we collect 'ba' or 'ab' based on x>y or y>x
+  let score = 0;
+  let n = s.length;
+  if (x >= y) {
+    let obj = removePairs(s, "b", "a", x);
+    // console.log("string ",s);
+    let obj1 = removePairs(obj.res, "a", "b", y);
+    score += obj.scr + obj1.scr;
+  } else {
+    let obj = removePairs(s, "a", "b", y);
+    // console.log("string ",s);
+    let obj1 = removePairs(obj.res, "b", "a", x);
+    score += obj.scr + obj1.scr;
+  }
+
+  return score;
+};
+
+function removePairs(str, target, check, point) {
+  console.log(str);
+  let st = [];
+  let score = 0;
+  let n = str.length;
+  let j = 0;
+  while (j < n) {
+    if (str[j] != target) {
+      st.push(str[j]);
+      j++;
+    } else {
+      if (st.length > 0 && st[st.length - 1] == check) {
+        st.pop();
+        score += point;
+        j++;
+      } else {
+        st.push(str[j]);
+        j++;
+      }
+    }
+  }
+
+  // console.log(st);
+  let res = "";
+  let i = 0;
+  while (i < st.length) {
+    res += st[i];
+    i++;
+  }
+  // console.log(res);
+
+  return { scr: score, res: res };
+}
