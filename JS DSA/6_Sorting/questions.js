7981d655ea056b577448cef53672b3864bb3d5d4
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-07-23 11:36:59 +0530

    Sorting extra Question added

--- JS DSA/6_Sorting/questions.js
+++ JS DSA/6_Sorting/questions.js
@@ -427,3 +427,36 @@ Answer is minimum of all these cases!
 
   return minDiff;
 };
+
+/*
+Sort Array by Increasing Frequency
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order. Return the sorted array.
+
+Example 1:
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+
+Example 2:
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+*/
+var frequencySort = function (nums) {
+  // Make a map
+  // Store the frequency of elements
+  // Sort the elements based on frequency in descending order
+  // if 2 elements have same frequency, sort them on descending order
+  let mpp = new Map();
+  let n = nums.length;
+  for (let i = 0; i < n; i++) {
+    let leftOver = mpp.get(nums[i]) || 0;
+    mpp.set(nums[i], leftOver + 1);
+  }
+
+  nums.sort((a, b) => {
+    return mpp.get(a) - mpp.get(b) || b - a;
+  });
+
+  return nums;
+};
