734fa92559b37938a89f8e616ec6aa1f4999b25b
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-07-24 09:42:09 +0530

    Sorting extra Question

--- JS DSA/6_Sorting/questions.js
+++ JS DSA/6_Sorting/questions.js
@@ -460,3 +460,73 @@ var frequencySort = function (nums) {
 
   return nums;
 };
+
+// Sort the Jumbled Numbers [Important] [Check the mapping concept of picking each digit of a number from beginning instead of num*10 + last%10 vala concept]
+/*
+You are given a 0-indexed integer array mapping which represents the mapping rule of a shuffled decimal system. mapping[i] = j means digit i should be mapped to digit j in this system.
+
+The mapped value of an integer is the new integer obtained by replacing each occurrence of digit i in the integer with mapping[i] for all 0 <= i <= 9.
+
+You are also given another integer array nums. Return the array nums sorted in non-decreasing order based on the mapped values of its elements.
+
+Notes:
+
+Elements with the same mapped values should appear in the same relative order as in the input.
+The elements of nums should only be sorted based on their mapped values and not be replaced by them.
+
+Example 1:
+Input: mapping = [8,9,4,0,2,1,3,5,7,6], nums = [991,338,38]
+Output: [338,38,991]
+Explanation:
+Map the number 991 as follows:
+1. mapping[9] = 6, so all occurrences of the digit 9 will become 6.
+2. mapping[1] = 9, so all occurrences of the digit 1 will become 9.
+Therefore, the mapped value of 991 is 669.
+338 maps to 007, or 7 after removing the leading zeros.
+38 maps to 07, which is also 7 after removing leading zeros.
+Since 338 and 38 share the same mapped value, they should remain in the same relative order, so 338 comes before 38.
+Thus, the sorted array is [338,38,991].
+
+Example 2:
+Input: mapping = [0,1,2,3,4,5,6,7,8,9], nums = [789,456,123]
+Output: [123,456,789]
+Explanation: 789 maps to 789, 456 maps to 456, and 123 maps to 123. Thus, the sorted array is [123,456,789].
+*/
+var sortJumbled = function (mapping, nums) {
+  // mapping = [8,9,4,0,2,1,3,5,7,6] means 0 -> 8, 1 -> 9, 2 -> 4, 3->0........9->6
+  // we need to replace all numbers in nums with its mapping values and then return an array with sorted values in ascending order
+  // if 2 values have same mapping value, return in same relative order like In [991,338,38], 338 and 38 both corresponds to 007 mapping value so return in same order [338,38] in answer array
+  // mapping each number with its mapping
+  // storing all original and mapped value in a map
+  // sorting the map based on mapped values
+  // storing all original values from sorted map
+  // To store mapped values, we take each digit from a number one by one using split and join function to convert it to a string, then splitting it to an array and then return it as a string with its mapped values
+  let mpp = mappingEachNumber(mapping, nums);
+  return mpp;
+};
+
+function mappingEachNumber(mapping, nums) {
+  let mpp = {};
+  // [ '06655', '95749', '984313', '030', '53', '39257162', '070' ]
+  let arr = nums.map((digit) => {
+    // taking each element of nums, splitting it and storing its mapped values and then again joining it.
+    return digit
+      .toString()
+      .split("")
+      .map((ele) => {
+        return mapping[ele];
+      })
+      .join("");
+  });
+
+  // storing all original and mapped inside an object
+  let mappedValues = nums.map((digit, index) => {
+    return { original: digit, mapped: arr[index] };
+  });
+
+  // osrt based on mapped values
+  mappedValues.sort((a, b) => a.mapped - b.mapped);
+
+  // returning all mapped values
+  return mappedValues.map((digit) => digit.original);
+}
