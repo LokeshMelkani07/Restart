6920a9cd9ab884c0fa8263787af2fd0602b65980
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-08-04 10:18:35 +0530

    SLiding window extra question added

--- JS DSA/12_SlidingWindow_2Pointer/index.js
+++ JS DSA/12_SlidingWindow_2Pointer/index.js
@@ -736,3 +736,43 @@ var minSwaps = function (nums) {
 
   return min;
 };
+
+// Range Sum of Sorted Subarray Sums
+/*
+You are given the array nums consisting of n positive integers. You computed the sum of all non-empty continuous subarrays from the array and then sorted them in non-decreasing order, creating a new array of n * (n + 1) / 2 numbers.
+
+Return the sum of the numbers from index left to index right (indexed from 1), inclusive, in the new array. Since the answer can be a huge number return it modulo 109 + 7.
+
+Example 1:
+Input: nums = [1,2,3,4], n = 4, left = 1, right = 5
+Output: 13
+Explanation: All subarray sums are 1, 3, 6, 10, 2, 5, 9, 3, 7, 4. After sorting them in non-decreasing order we have the new array [1, 2, 3, 3, 4, 5, 6, 7, 9, 10]. The sum of the numbers from index le = 1 to ri = 5 is 1 + 2 + 3 + 3 + 4 = 13.
+*/
+var rangeSum = function (nums, n, left, right) {
+  // Taking subarrays sum, putting in an array, sort it, then getting sum from left to right and returning it
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    // pushing numbers itself as they also are individual subarrays
+    res.push(nums[i]);
+  }
+
+  for (let i = 0; i < n; i++) {
+    let j = i + 1;
+    let sum = nums[i];
+    while (j < n) {
+      sum += nums[j];
+      res.push(sum);
+      j++;
+    }
+  }
+
+  res.sort((a, b) => a - b);
+  let ans = 0;
+  for (let i = left - 1; i < right; i++) {
+    // As left and right are 1-based indexed so we do left-1 and <right
+    ans += res[i];
+  }
+
+  // % (1e9 + 7) is given so we have to do it
+  return ans % (1e9 + 7);
+};
